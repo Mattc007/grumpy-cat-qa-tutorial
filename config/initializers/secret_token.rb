e73154ff9f365ff8e6a17a975c1600d8f5487f04
Matt Catrett <mcatrett@turbosquid.com>
2014-07-21 17:19:39 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrumpyCatQaTutorial::Application.config.secret_key_base = 'd8ec7a1e104cee12c01bce0d62d227d4587cd5e15d042732f2499a00a623962764d1bfa78b8b09a492d1d9e3d074b4e09ca275d20354b6c7b3835cb9ef20fd96'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GrumpyCatQaTutorial::Application.config.session_store :cookie_store, key: '_grumpy-cat-qa-tutorial_session'
